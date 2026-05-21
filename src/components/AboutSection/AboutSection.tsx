cc804868588dfe2b5102c7eb60f895aec64e8449
AntonyAjay123 <ajaygeoffrey456@gmail.com>
2026-05-21 11:28:54 +0100

    create about section

---
+++ src/components/AboutSection/AboutSection.tsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+import './AboutSection.scss'
+export const AboutSection = () =>{
+    return <div className="about-section">
+
+        <div className="section-title-container">
+            <h1 className="section-title"> About<span className="title-us">Us</span></h1>
+        </div>
+        <div className="about-content">
+            DY Prime Properties LLP started its maiden project, Nandi Magnolia, a gated community in Bengaluru at the foot of the Nandi hills. The quality and aesthetics of our first project were so good that it was a sell-out. The immense goodwill we generated among our customers encouraged us to launch multiple projects. We operate with the highest integrity and character in delivering what we promise. We walk the extra mile to satisfy our customers' needs. We take pride and joy in enriching the lives we touch by doing things the right way.
+    </div>
+
+    </div>
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import { AboutSection } from "../components/AboutSection/AboutSection"
 import { HomeSlider } from "../components/CustomCarousel/CustomCarousel"
 
 export const Home = ()=>{
     return(
         <>
         <HomeSlider/>
+        <AboutSection/>
         </>
     )
 }
